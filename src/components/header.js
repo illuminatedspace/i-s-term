5c287e103fd9149e713093dba365eae4317c4927
illuminatedspace <lizkristinaphillips@gmail.com>
2019-01-12 20:06:48 -0500

    feat: add first styled component to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const NavLi = styled.li`
+  list-style-type: none;
+  display: inline;
+  margin: 5px;
+`
+
+const Header = ({ siteTitle, menuLinks }) => (
+  <nav>
+    {menuLinks.map(({ link, name }) => (
+      <NavLi key={name}>
+        <Link to={link}>{name}</Link>
+      </NavLi>
+    ))}
+  </nav>
 )
 
 export default Header
