342100ad2ed3dc1eed4aaa123296fbcf169755e7
illuminatedspace <lizkristinaphillips@gmail.com>
2019-01-12 20:05:07 -0500

    fix: command line component

--- src/components/CommandLine.js
+++ src/components/CommandLine.js
@@ -2,14 +2,67 @@ import React from 'react'
 
 import TextInput from './TextInput'
 
+const commands = {
+  help: {
+    name: 'help',
+    description: 'shows all commands avalible',
+    flag: '-h',
+  },
+  command2: {
+    name: 'command2',
+    description: 'another command!',
+    flag: '-c',
+  },
+}
+
+/**
+ * Get response for command given
+ * @param {string} command
+ * @returns {string} response
+ */
+const getResponse = command => {
+  switch (command) {
+    case commands.help.flag:
+    case commands.help.name:
+      /*
+        should return format for every key in commands 
+        help -h
+        shows all commands available
+      */
+      const helpText = Object.values(commands).reduce(
+        (acc, { name, flag, description }) => {
+          const commandEntry = [`${name}, ${flag}`, description]
+          return acc.concat(commandEntry)
+        },
+        ['Here are all the commands available:']
+      )
+
+      return helpText.join('\n')
+
+    default:
+      return 'Sorry. I did not understand that command.\nType "help" to see a list of accepted commands'
+  }
+}
+
 class CommandLine extends React.Component {
   state = {
     lines: ['hello world', 'print hello', ''],
   }
 
+  /**
+   * takes a line or array of lines and adds it to this.state.lines
+   * @param {string|array} line line or lines to add
+   * @fires setState
+   */
   addLine = line => {
-    console.log('MACHOP!')
     this.setState({ lines: this.state.lines.concat(line) })
+  }
+
+  parseCommand = command => {
+    // match command to response (switch)
+    const response = getResponse(command)
+    // add line for both command and response
+    this.addLine([command, response])
   }
 
   render() {
@@ -18,7 +71,7 @@ class CommandLine extends React.Component {
         {this.state.lines.map((line, index) => (
           <p key={index}>{`> ${line}`}</p>
         ))}
-        <TextInput addLine={this.addLine} handleSubmit={this.handleSubmit} />
+        <TextInput parseCommand={this.parseCommand} />
       </div>
     )
   }
