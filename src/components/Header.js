39c8c9538d78ed7fdf5870f79d5ae32f64d9c231
illuminatedspace <lizkristinaphillips@gmail.com>
2019-01-26 18:28:21 -0500

    style refactor: use theme provider in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,8 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
 
-import { background, purple, yellow } from '../styles/colors'
-
 const Nav = styled.nav`
-  background-color: ${purple.light};
+  background-color: ${props => props.theme.accent.primary};
 `
 
 const NavLi = styled.li`
@@ -17,14 +15,14 @@ const NavLi = styled.li`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: ${background};
+  color: ${props => props.theme.background};
 
   :active {
     color: #fff;
   }
 
   :hover {
-    color: ${yellow.medium};
+    color: ${props => props.theme.highlight};
   }
 `
 
