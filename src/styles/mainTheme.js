dd78a46118999e9e66dc6f815b3d391f4f6041fa
illuminatedspace <lizkristinaphillips@gmail.com>
2019-01-26 18:26:35 -0500

    style: add main theme file

---
+++ src/styles/mainTheme.js
@@ -0,0 +1,15 @@
+import { yellow, background, purple } from './colors'
+
+const mainTheme = {
+  text: {
+    primary: yellow.medium,
+    accent: background,
+  },
+  background: background,
+  highlight: yellow.medium,
+  accent: {
+    primary: purple.light,
+  },
+}
+
+export default mainTheme
