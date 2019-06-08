e4d6bf72c383322b6877538636ad61916b81f7ff
Liz Phillips <lizkristinaphillips@gmail.com>
2019-06-08 11:58:06 -0400

    feat: add about window

---
+++ src/components/windows/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Window from '../Window/Window'
+import { windowNames } from './_consts'
+
+const About = () => (
+  <Window windowName={windowNames.about}>{"Hi, I'm Liz"}</Window>
+)
+
+export default About
