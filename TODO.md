66b785cb2508acd9eccf9a1f93103bbe4f1bdbdb
Liz Phillips <lizkristinaphillips@gmail.com>
2019-06-08 11:31:25 -0400

    docs: add seperate todo list

---
+++ TODO.md
@@ -0,0 +1,11 @@
+TODO:
+
+- [x] title underneath
+- [x] hover state
+- [] double click behavior
+  - [] convert Main to class
+  - [] create launchWindow fn in Main
+  - [] pass launchWindow down to the icon
+  - [] listen for double click
+  - [] on double click invoke launchWindow
+- [] drag and drop behavior?
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -48,15 +48,4 @@ class Icon extends React.Component {
   }
 }
 
-// TODO:
-// - [x] title underneath
-// - [x] hover state
-// - [] double click behavior
-//   - [] convert Main to class
-//   - [] create launchWindow fn in Main
-//   - [] pass launchWindow down to the icon
-//   - [] listen for double click
-//   - [] on double click invoke launchWindow
-// - [] drag and drop behavior?
-
 export default Icon
