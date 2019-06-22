13bbfdeb73a808e3fc6725e8b895856f6583d344
Liz Phillips <lizkristinaphillips@gmail.com>
2019-06-22 11:59:45 -0400

    refactor: use window names

--- src/components/windows/components/About.js
+++ src/components/windows/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Window from '../Window/Window'
-import { windowNames } from './_consts'
+import Window from '../../Window/Window'
+import { windowNames } from '../_consts'
 
 const About = () => (
   <Window windowName={windowNames.about}>{"Hi, I'm Liz"}</Window>
