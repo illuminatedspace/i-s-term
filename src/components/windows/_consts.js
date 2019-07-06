d76ace36c1fc9713c9c1ac822d6fbbb59b4e4840
Liz Phillips <lizkristinaphillips@gmail.com>
2019-07-06 10:24:59 -0400

    refactor: remove windowToIconMap

--- src/components/windows/_consts.js
+++ src/components/windows/_consts.js
@@ -2,10 +2,6 @@ const about = 'About'
 
 export const windowNames = { about }
 
-export const windowToIconMap = {
-  [windowNames.about]: 'crystal-ball',
-}
-
 export const iconStates = {
   active: 'active',
   inactive: 'inactive',
