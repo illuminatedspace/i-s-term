5344cf6b346fc3b4b59290a45969458d176b7149
Liz Phillips <lizkristinaphillips@gmail.com>
2019-07-13 11:35:44 -0400

    feat: WIP add projects placeholder window

---
+++ src/components/windows/components/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Window from '../../Window/Window'
+import { windowNames } from '../_consts'
+
+const Projects = () => (
+  <Window windowName={windowNames.projects}>{'These are projects.'}</Window>
+)
+
+export default Projects
