fddaefb64d28f7236e675401f8533431ba7d8ed4
Liz Phillips <lizkristinaphillips@gmail.com>
2019-08-10 11:22:19 -0400

    docs: deprecate code and add comments

--- src/components/windows/components/Terminal/TerminalText.js
+++ src/components/windows/components/Terminal/TerminalText.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 
+// REQUIREMENTS
 // list all commands in a formatted way
 // format all commands and responses with different colors
 // every "text block" needs to have new lines and margin bottom
 
-const getMarginBottomDiv = textType => styled.div`
-  margin-bottom: 1em;
-  // color: ${props => props.theme.text[textType]};
-`
+// DEPRECATED
+// const getMarginBottomDiv = textType => styled.div`
+//   margin-bottom: 1em;
+//   // color: ${props => props.theme.text[textType]};
+// `
+
+// DEPRECATED
+// const getDivByTextNodeType = textNodeType => {
+//   const textType = textNodeType === 'command' ? 'quadentiary' : 'secondary'
+
+//   return getMarginBottomDiv(textType)
+// }
 
 const MarginBottomDiv = styled.div`
   margin-bottom: 1em;
 `
-
-const getDivByTextNodeType = textNodeType => {
-  const textType = textNodeType === 'command' ? 'quadentiary' : 'secondary'
-
-  return getMarginBottomDiv(textType)
-}
 
 const NoMarginBottomParagraph = styled.p`
   margin-bottom: 0;
