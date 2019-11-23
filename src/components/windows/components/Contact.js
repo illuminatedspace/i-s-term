e156480b18f034a7ceaa472a0bb454f919ee69aa
Liz Phillips <lizkristinaphillips@gmail.com>
2019-11-23 14:41:03 -0500

    feat: fill out contact page

--- src/components/windows/components/Contact.js
+++ src/components/windows/components/Contact.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import Window from '../../Window/Window'
 import { windowNames } from '../_consts'
+import { StyledLink, PaddedWindowDiv } from '../../styled'
 
 const Contact = ({ makeWindowActive }) => (
-  <Window makeWindowActive={makeWindowActive} windowName={windowNames.Contact}>
-    {'Contact me: liz@liz.liz'}
+  <Window
+    makeWindowActive={makeWindowActive}
+    windowName={windowNames.Contact}
+    shouldScroll={true}
+  >
+    <PaddedWindowDiv>
+      <p>
+        The best way to reach me is through my email address:
+        LizKristinaPhillips [at] gmail [dot] com.
+      </p>
+
+      <p>
+        Click the link below to start a new email to me in your default mail
+        client if you have it enabled on your device.
+      </p>
+
+      <StyledLink
+        href="mailto:lizkristinaphillips@gmail.com"
+        target="blank"
+        rel="noopener noreferrer"
+      >
+        Send Me an Email
+      </StyledLink>
+    </PaddedWindowDiv>
   </Window>
 )
 
