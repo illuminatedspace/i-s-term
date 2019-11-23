e5b6bf1e3241cf440d05e3d4adb929f98405bf44
Liz Phillips <lizkristinaphillips@gmail.com>
2019-11-23 12:24:50 -0500

    feature: create two types of styled windows

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -5,6 +5,9 @@ import styled from 'styled-components'
 import WindowBar from './WindowBar'
 import { dragHandleClassName } from '../../styles'
 
+const createStyledDiv = shouldScroll =>
+  shouldScroll ? ScrollingStyledDiv : StyledDiv
+
 const StyledDiv = styled.div`
   border: ${props => props.theme.window.border};
   font-family: ${props => props.theme.fontFamily};
@@ -14,12 +17,35 @@ const StyledDiv = styled.div`
   width: 100%;
 `
 
-const Window = ({ children, windowName }) => (
-  <>
-    <WindowBar windowName={windowName} />
-    <StyledDiv>{children}</StyledDiv>
-  </>
-)
+const ScrollingStyledDiv = styled.div`
+  border: ${props => props.theme.window.border};
+  font-family: ${props => props.theme.fontFamily};
+  background-color: ${props => props.theme.window.background};
+  color: ${props => props.theme.window.text};
+  height: calc(100% - 35px);
+  width: 100%;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    background-color: ${props => props.theme.window.bar};
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${props => props.theme.window.bar};
+    border-left: 1px solid ${props => props.theme.window.bar};
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${props => props.theme.accent.secondary};
+  }
+`
+
+const Window = ({ children, windowName, shouldScroll }) => {
+  const DivComponent = createStyledDiv(shouldScroll)
+  return (
+    <>
+      <WindowBar windowName={windowName} />
+      <DivComponent>{children}</DivComponent>
+    </>
+  )
+}
 
 const defaultStartingPosition = {
   x: 250,
